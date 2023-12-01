cb1dcad00a0e309b7c32c5160d0fa7afd929bcb1
aayushi <aayushi.fichadiya@gmail.com>
2023-12-01 12:07:20 +0530

    search api

--- Auth/apps/project/views.py
+++ Auth/apps/project/views.py
@@ -79,7 +79,7 @@ async def update_project(
     )
 
 #search project
-@project_router.get("/projects/search/", response_model=List[ProjectResponsef])
+@project_router.get("/projects/search/", response_model=List[ProjectResponse])
 def search_projects(name: str, db: Session = Depends(get_session)):
     projects = db.query(Project).filter(Project.name.ilike(f"%{name}%")).all()
     return projects
